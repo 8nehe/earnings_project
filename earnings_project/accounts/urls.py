21cfb5955baeae3c62104517bd5ace8ea664fdf7
Nehemiah Kamolu <nkamolu@alxafrica.com>
2025-08-21 16:47:47 +0300

    add hello worls endpoint

--- earnings_project/accounts/urls.py
+++ earnings_project/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path("register/", views.UserRegistrationView.as_view(), name="user-registration")
+    path("register/", views.UserRegistrationView.as_view(), name="user-registration"),
+    path("", views.hello_world, name="hello-world"),
 ]
